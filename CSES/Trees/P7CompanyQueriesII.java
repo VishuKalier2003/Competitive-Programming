48866b8d23e443700ce7aa023b26ba1769766ca0
VishuKalier2003 <vishukalier123@gmail.com>
2025-05-16 06:47:39 +0530

    update v1

--- CSES/Trees/P7CompanyQueriesII.java
+++ CSES/Trees/P7CompanyQueriesII.java
@@ -8,12 +8,12 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.List;
 
-public class P6CompanyQueriesI {
+public class P7CompanyQueriesII {
     public static class FastReader {
         private final byte buffer[] = new byte[1 << 20];
         private int ptr = 0, len = 0;
 
-        private int read() throws IOException {
+        public int read() throws IOException {
             if(ptr >= len) {
                 ptr = 0;
                 len = System.in.read(buffer);
@@ -33,7 +33,7 @@ public class P6CompanyQueriesI {
             if(neg)
                 c = read();
             do {
-                x = 10 * x + (c-'0');
+                x = x * 10 + (c - '0');
             } while((c = read()) >= '0' && c <= '9');
             return neg ? -x : x;
         }
@@ -42,22 +42,22 @@ public class P6CompanyQueriesI {
             int c;
             long x = 0l;
             while((c = read()) <= ' ')
-                if(c < 0)
+                if(c <= 0)
                     return -1;
             boolean neg = c == '-';
             if(neg)
                 c = read();
             do {
-                x = x * 10 + (c-'0');
+                x = x * 10 + (c - '0');
             } while((c = read()) >= '0' && c <= '9');
-            return x;
+            return neg ? -x : x;
         }
     }
 
     public static List<List<Integer>> tree;
     public static List<int[]> queries;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         FastReader fast = new FastReader();
         final int n = fast.nextInt(), q = fast.nextInt();
         tree = new ArrayList<>();
@@ -79,36 +79,38 @@ public class P6CompanyQueriesI {
         final StringBuilder output = new StringBuilder();
         final PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out));
         for(int query[] : queries)
-            output.append(sparseTable.lifting(query[0], query[1])).append("\n");
+            output.append(sparseTable.lca(query[0], query[1])).append("\n");
         writer.write(output.toString());
         writer.flush();
     }
 
     public static final class SparseTable {
         public int lift[][];
+        public int depth[];
         public int maxLog;
 
         public SparseTable(final int n) {
             this.maxLog = 32 - Integer.numberOfLeadingZeros(n);
-            // Note: for binary lifting always use maxLog+1 and n+1 (root node is generally 1 based)
             this.lift = new int[maxLog+1][n+1];
+            this.depth = new int[n+1];
             for(int row[] : lift)
                 Arrays.fill(row, -1);
             binaryLiftingIterative();
         }
 
         public void binaryLifting(int root, int parent) {
-            this.lift[0][root] = parent;        // Info: Fill the immediate parent
+            this.lift[0][root] = parent;
             for(int k = 1; k <= this.maxLog; k++) {
-                // Note: When previous power of two is already computed (k-1 != -1), then we can jump to next power (k) by jumping (k-1) power twice
                 if(this.lift[k-1][root] != -1)
                     this.lift[k][root] = this.lift[k-1][this.lift[k-1][root]];
                 else
                     this.lift[k][root] = -1;
             }
             for(int child : tree.get(root))
-                if(child != parent)
+                if(child != parent) {
+                    this.depth[child] = this.depth[root] + 1;
                     binaryLifting(child, root);
+                }
         }
 
         public void binaryLiftingIterative() {
@@ -117,7 +119,14 @@ public class P6CompanyQueriesI {
             while(!stack.isEmpty()) {
                 int data[] = stack.pop();
                 int root = data[0], parent = data[1];
-                this.lift[0][root] = parent;
+                if(parent == -1) {
+                    this.depth[root] = 0;
+                    this.lift[0][root] = root;
+                }
+                else {
+                    this.depth[root] = this.depth[parent] + 1;
+                    this.lift[0][root] = parent;
+                }
                 for(int k = 1; k <= this.maxLog; k++) {
                     if(this.lift[k-1][root] != -1)
                         this.lift[k][root] = this.lift[k-1][this.lift[k-1][root]];
@@ -132,14 +141,29 @@ public class P6CompanyQueriesI {
 
         public int lifting(int root, int k) {
             int ancestor = root;
-            for(int i = 0; i <= this.maxLog; i++) {
-                if((k & (1 << i)) != 0) {       // Note: Checking if i-th bit is set
+            for(int i = 0; i <= this.maxLog; i++)
+                if((k & (1 << i)) != 0) {
                     ancestor = this.lift[i][ancestor];
-                    if(ancestor == -1)      // If next higher ancestor is not there
+                    if(ancestor == -1)
                         break;
                 }
-            }
             return ancestor;
+        }
+
+        public int lca(int x, int y) {
+            if(this.depth[x] > this.depth[y])
+                x = lifting(x, this.depth[x] - this.depth[y]);
+            else if(this.depth[y] > this.depth[x])
+                y = lifting(y, this.depth[y] - this.depth[x]);
+            if(x == y)
+                return x;
+            for(int k = this.maxLog; k >= 0; k--) {
+                if(this.lift[k][x] != -1 && this.lift[k][x] != this.lift[k][y]) {
+                    x = this.lift[k][x];
+                    y = this.lift[k][y];
+                }
+            }
+            return this.lift[0][x];
         }
     }
 }
