388c44a2fcad5544b4506bd4ebd800f9b9f8d6a2
VishuKalier2003 <vishukalier123@gmail.com>
2025-08-04 13:46:46 +0530

    2-SAT-CSES

--- CSES/Graph/P26GinatPizza.java
+++ CSES/Graph/P26GinatPizza.java
@@ -128,20 +128,25 @@ public class P26GinatPizza {
             impGraph.add(new ArrayList<>());
             impGraphReverse.add(new ArrayList<>());
         }
+        // Micro-optimisation: creating 2m nodes and storing first m as +ve and next m as -ve
         for (int i = 0; i < n; i++) {
             char ch1 = fr.next().charAt(0);
             int x = fr.nextInt();
             char ch2 = fr.next().charAt(0);
             int y = fr.nextInt();
+            // Info: The distance between x and ~x is m
             int u = (ch1 == '+') ? x : x + m;
             int v = (ch2 == '+') ? y : y + m;
-            int notU = (u > m) ? u - m : u + m;
+            // If u is greater than m, then I need to shift u to left by m (u-m), else right by m (u+m)
+            int notU = (u > m) ? u - m : u + m;     // shifting to get the opposite sign
             int notV = (v > m) ? v - m : v + m;
-            impGraph.get(notU).add(v);
+            // Info: If the clause is (a or b) then it can be implied into ~a -> b and ~b -> a (two implications)
+            impGraph.get(notU).add(v);      // Original Implication graph
             impGraph.get(notV).add(u);
-            impGraphReverse.get(v).add(notU);
+            impGraphReverse.get(v).add(notU);       // reverse Implication graph
             impGraphReverse.get(u).add(notV);
         }
+        // Info: All these clauses are merged by AND into a Conjunctive Normal Form (CNF) via the constraints
         fw.attachOutput(solve(n, m));
         fw.printOutput();
     }
@@ -152,13 +157,17 @@ public class P26GinatPizza {
 
     public static StringBuilder solve(final int n, final int m) {
         stack = new Stack<>();
-        sccFlag = true;
-        int sccNodes[] = Kosaraju(m);
+        sccFlag = true;     // Flag to check if all SCC formed are valid
+        int sccNodes[] = Kosaraju(m);       // Running the Kosraju algorithm to define all SCC
         if(!sccFlag)
             return new StringBuilder().append("IMPOSSIBLE");
         char result[] = new char[m+1];
+        // Now for every variable (1 to m)
         for(int i = 1; i <= m; i++) {
-            int positive = sccNodes[i], negative = sccNodes[i+m];
+            // Get the SCC indexes for x and ~x
+            int positive = sccNodes[i];     // Taking
+            int negative = sccNodes[i+m];   // Not Taking
+            // Note: Intuitively, the later the literal appears in topological order, the more freely we can take it (Assign a +ve), since it has lesser constraints
             result[i] = positive > negative ? '+' : '-';
         }
         final StringBuilder output = new StringBuilder();
@@ -167,26 +176,29 @@ public class P26GinatPizza {
         return new StringBuilder().append(output);
     }
 
+    // Note: A higher SCC index means that node is coming later than other node in Original graph, since Kosaraju processes then in topological order
     public static int[] Kosaraju(final int m) {
-        boolean visited[] = new boolean[2*m+1];
+        boolean visited[] = new boolean[2*m+1];     // Creating visited array (2m+1) nodes for 1 based indexing
         int sccNodes[] = new int[2*m+1];
         component = new HashSet<>();
+        // Assuming the graph is disconnected, running dfs for each component
         for (int i = 1; i <= 2*m; i++) {
             if (!visited[i])
                 firstDfs(i, visited);
         }
-        Arrays.fill(visited, false);
+        Arrays.fill(visited, false);        // resetting the visited array
         sccIndex = 1;
         while(!stack.isEmpty()) {
             int x = stack.pop();
-            if(visited[x])
+            if(visited[x])      // If visited, skip it
                 continue;
-            component.clear();
-            if(!secondDfs(x, m, visited)) {
+            component.clear();      // resetting the current SCC (ready to make new one)
+            if(!secondDfs(x, m, visited)) {     // running second dfs on reverse Implication graph
                 sccFlag = false;
                 return sccNodes;
             }
-            for(int v : component)
+            // Note: Kosraju porcesses SCC in such a way that lower Id SCC are sinks and higher Id SCC are sources
+            for(int v : component)      // Info: giving the current SCC nodes the same SCC index
                 sccNodes[v] = sccIndex;
             sccIndex++;
         }
@@ -194,31 +206,33 @@ public class P26GinatPizza {
     }
 
     private static Set<Integer> component;
-    private static int sccIndex = 1;
+    private static int sccIndex = 1;        // Index for indexing the SCC
 
     public static void firstDfs(int node, boolean visited[]) {
-        if (visited[node])
+        if (visited[node])      // If visited node found backtrack
             return;
         visited[node] = true;
         for (int neighbor : impGraph.get(node))
             if (!visited[neighbor])
-                firstDfs(neighbor, visited);
+                firstDfs(neighbor, visited);        // Traversing unvisited paths
         stack.push(node);
     }
 
+    // We add nodes in post order to ensure that the popped node is node whose entire SCC is reachable in reverse graph
     public static boolean secondDfs(int node, int m, boolean visited[]) {
-        if(visited[node])
+        if(visited[node])       // If visited node found backtrack
             return true;
         visited[node] = true;
         component.add(node);
         boolean isValid = true;
         int opposite = node > m ? node-m : node+m;
-        // Info: SCC is not valid if x and ~x are in same SCC
+        // Info: SCC is not valid if x and ~x are in same SCC because Implications always implies True, and in SCC we can reach from u to v and v to u, so x and ~x cannot be same
         if(component.contains(opposite))
             return false;
         for(int neighbor : impGraphReverse.get(node))
             if(!visited[neighbor])
-                isValid &= secondDfs(neighbor, m, visited);
+                // AND operator to ensure each node is valid in SCC
+                isValid &= secondDfs(neighbor, m, visited);     // Traversing unvisited paths
         return isValid;
     }
 }
