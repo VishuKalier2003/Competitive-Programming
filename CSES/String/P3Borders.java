3da2e2c84bcc8a07f098aedbb09dfd1dee7504f3
VishuKalier2003 <vishukalier123@gmail.com>
2025-09-10 22:55:19 +0530

    CSES-strings

---
+++ CSES/String/P3Borders.java
@@ -0,0 +1,162 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class P3Borders {
+    // Micro-optimisation: FastReader defined for fast input reading via byte buffer
+    public static class FastReader {
+        // Creates a 1MB buffer such that 1MB of data is stored
+        private static final byte[] buffer = new byte[1 << 20];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException { // reading byte
+            if (ptr >= len) {
+                ptr = 0;
+                // len marks the length of the last unchecked index in the buffer
+                len = System.in.read(buffer); // Stores the entire buffer data in read
+                if (len <= 0)
+                    return -1;
+            }
+            // Extract buffer and move pointer to next without calling System.in.read()
+            return buffer[ptr++] & 0xff;
+        }
+
+        public int nextInt() throws IOException { // reading int
+            int x = 0, c;
+            while ((c = read()) <= ' ') // While whitespace is not provided
+                if (c < 0)
+                    return -1;
+            boolean neg = c == '-';
+            if (neg)
+                c = read();
+            do {
+                x = 10 * x + (c - '0');
+            } while ((c = read()) >= '0' && c <= '9');
+            return neg ? -x : x;
+        }
+
+        public long nextLong() throws IOException { // reading long
+            long x = 0l, c;
+            while ((c = read()) <= ' ')
+                if (c < 0)
+                    return -1;
+            boolean neg = c == '-';
+            if (neg)
+                c = read();
+            do {
+                x = 10 * x + (c - '0');
+            } while ((c = read()) >= '0' && c <= '9');
+            return neg ? -x : x;
+        }
+
+        public String next() throws IOException { // reading string (whitespace exclusive)
+            int c;
+            while ((c = read()) <= ' ') // Read until whitespace
+                if (c < 0)
+                    return null;
+            StringBuilder sb = new StringBuilder();
+            do {
+                sb.append((char) c);
+            } while ((c = read()) > ' ');
+            return sb.toString();
+        }
+
+        public String nextLine() throws IOException { // reading string (whitespace inclusive)
+            StringBuilder sb = new StringBuilder();
+            int c = read();
+            if (c < 0)
+                return null;
+            while (c != '\n' && c >= 0) {
+                if (c != '\r')
+                    sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+    }
+
+    // Micro-optimisation: FastWriter class to reduce flushes in each write
+    public static class FastWriter {
+        public PrintWriter pw;
+        public StringBuilder sb;
+
+        public FastWriter() {
+            this.pw = new PrintWriter(new OutputStreamWriter(System.out));
+            this.sb = new StringBuilder();
+        }
+
+        public void attachOutput(StringBuilder s) {
+            sb.append(s);
+        }
+
+        public void printOutput() {
+            pw.write(sb.toString());
+            pw.flush();
+        }
+    }
+
+    // Micro-optimisation: creating new thread, not hitting MLE
+    public static void main(String[] args) {
+        Thread t = new Thread(null, () -> {
+            try {
+                callMain(args);
+            } catch (IOException e) {
+                e.getLocalizedMessage();
+            }
+        }, "Borders-(https://cses.fi/problemset/task/1732/)", 1 << 26);
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException iE) {
+            iE.getLocalizedMessage();
+        }
+    }
+
+    // Micro-optimisation: defining final variables stored in cache reducing time
+    public static void callMain(String args[]) throws IOException {
+        FastReader fr = new FastReader();
+        FastWriter fw = new FastWriter();
+        final String s = fr.next();
+        solve(s, s.length());
+        fw.attachOutput(output);
+        fw.printOutput();
+    }
+
+    private static final StringBuilder output = new StringBuilder();
+
+    public static void solve(final String s, final int n) {
+        int lps[] = lps(s, n);
+        List<Integer> border = new ArrayList<>();
+        int k = lps[n-1];       // The prefix suffix stored at n-1 index
+        while(k > 0) {
+            border.add(k);      // adding that as the border
+            k = lps[k-1];       // moving to the smaller border as the valid prefix suffix
+        }
+        Collections.reverse(border);
+        for(int b : border)
+            output.append(b).append(" ");
+    }
+
+    private static int[] lps(final String s, final int n) {
+        final int lps[] = new int[n];
+        int j = 0, i = 1;
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+                lps[i] = j;
+                i++;
+            } else {
+                if (j != 0)
+                    j = lps[j - 1];
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
